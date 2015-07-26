1d51978ce7b1fe3e964efab685085aec9fe52a1d
Niklas Haas <git@nand.wakku.to>
2015-07-26 16:22:25 +0200

    Implement rudimentary saving functionality

--- server.hs
+++ server.hs
@@ -1,9 +1,13 @@
+import Control.Exception;
+
 import System.Directory;
 import System.Environment;
 import System.Exit;
 import System.IO;
 import System.IO.Temp;
 import System.FilePath;
+import System.Posix.User;
+import System.Posix.Files;
 
 import qualified Data.ByteString.Lazy as LB
 
@@ -12,6 +16,11 @@ die :: IO ()
 die = do
     putStrLn "Segmentation fault"
     exitFailure
+
+putURL :: String -> IO ()
+putURL name = do
+    user <- getLoginName
+    putStrLn $ "https://☃.valk.nand.wakku.to/~" ++ user ++ "/" ++ name
 
 main :: IO ()
 main = do
@@ -25,16 +34,24 @@ go :: String -> String -> IO ()
 go "true"  _ = withInput smallestHash
 go "false" name
     | null name = die
-    | otherwise = withInput (tryRename name)
+    | otherwise = withInput $ \f -> do
+                    res <- trySave f name
+                    case res of
+                        Left e  -> print e >> die
+                        Right _ -> putURL name
 go _ _ = die
 
 withInput :: (FilePath -> IO ()) -> IO ()
 withInput callback = withTempFile ".." ".tmp" $ \f h -> do
+    hSetBinaryMode h True
     LB.hGetContents stdin >>= LB.hPut h
+    hClose h
     callback f
 
 smallestHash :: FilePath -> IO ()
 smallestHash _ = putStrLn "hash"
 
-tryRename :: String -> FilePath -> IO ()
-tryRename n f = putStrLn $ "renaming from " ++ f ++ " to " ++ n
+trySave :: FilePath -> FilePath -> IO (Either IOException ())
+trySave f n = try $ do
+    renameFile f n
+    setFileMode n 0o444
